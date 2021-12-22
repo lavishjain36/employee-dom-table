5fe24a878036a01757a4d250b26bd14b6bef8473
Lavish <lavishjain36@gmail.com>
2021-12-22 12:15:47 +0530

    DOm Prac

--- script.js
+++ script.js
@@ -1,67 +1,161 @@
-//Write a logic to search the data 
+let employees=[
+  {
+    id:1,
+    first_name:"Sourav",
+    last_name:"Das",
+    email:"sourav@gmail.com",
+    gender:"Male",
+    ip_address:"160.192.178.21"
+  },
 
-function search(){
-  const url="https://jsonplaceholder.typicode.com/users"
+  {
+    id:2,
+    first_name:"John",
+    last_name:"Paul",
+    email:"john@gmail.com",
+    gender:"Male",
+    ip_address:"160.292.178.21"
+  },
 
-  fetch(url)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(users){
-    //Call a function which take users as a parameter
-    getData(users);
-  })
-  //Error using catch block 
-  .catch(function(error){
-    console.log(error);
-  })
+
+  {
+    id:3,
+    first_name:"Pavan",
+    last_name:"Sai",
+    email:"pavan@gmail.com",
+    gender:"Male",
+    ip_address:"160.292.278.21"
+  },
+
+  {
+    id:4,
+    first_name:"Sangeetha",
+    last_name:"Paul",
+    email:"sangeetha@gmail.com",
+    gender:"Female",
+    ip_address:"160.092.378.21"
+  },
+  
+
+
+  {
+    id:5,
+    first_name:"Pavni",
+    last_name:"Dhillon",
+    email:"pavni@gmail.com",
+    gender:"Female",
+    ip_address:"160.082.378.21"
+  },
+
+
+  {
+    id:6,
+    first_name:"Tushar",
+    last_name:"Vinocha",
+    email:"tushar@gmail.com",
+    gender:"Male",
+    ip_address:"160.092.378.21"
+  },
+]
+
+
+//ALL the Employee Data 
+let allEmpButton=document.querySelector("#all-emp-btn");
+allEmpButton.addEventListener("click",function(){
+displayEmployees(employees)
+})
+
+
+//Male employee data 
+let maleEmpButton=document.querySelector("#male-emp-btn");
+maleEmpButton.addEventListener("click",function(){
+  //Logic to filter out male data 
+  let maleEmployee=employees.filter(function(employee){
+    return employee.gender==="Male"
+
+  });
+  //call the function 
+  displayEmployees(maleEmployee)
+})
+
+
+//Female
+
+let femaleEmpButton=document.querySelector("#female-emp-btn");
+femaleEmpButton.addEventListener("click",function(){
+  //Logic to filter out Female data 
+  let femaleEmployee=employees.filter(function(employee){
+    return employee.gender==="Female"
+
+  });
+  //call the function 
+  displayEmployees(femaleEmployee)
+})
+
+
+//Search Functionality
+let searchBox=document.querySelector("#search-box");
+
+searchBox.addEventListener("keyup",function(){
+ let textEntered= searchBox.value
+console.log(textEntered);
+
+
+//create an empty array 
+let filteredEmployees=[];
+if(textEntered!==""){
+  filteredEmployees=employees.filter(function(employee){
+  return employee.first_name.toUpperCase().startsWith(textEntered.toUpperCase())
+  });
+  document.querySelector("#table-body").innerHTML="";
+  displayEmployees(filteredEmployees);
+
+}else{
+  document.querySelector("#table-body").innerHTML="";
 }
 
-
-
-function getData(users){
-//will use div which contains empty result
-let userDiv=document.querySelector("#users")
-userDiv.innerHTML="";
-
-
-//create a table element 
-let table =document.createElement("table")
-
-
-//Iterate over user data 
-users.forEach(function(currentUser){
-  //create a row 
-  let row =table.insertRow();
- let name= row.insertCell()
- name.innerHTML=currentUser.name;
-
- let city=row.insertCell()
-  city.innerHTML=currentUser.address.city;
-
-  let email=row.insertCell()
-  email.innerHTML=currentUser.email;
-
-  let phone=row.insertCell()
-  phone.innerHTML=currentUser.phone;
-
-  let website=row.insertCell()
-  website.innerHTML=currentUser.website;
-
-  let company=row.insertCell()
-  company.innerHTML=currentUser.company.name;
-
-  let company_catch=row.insertCell()
-  company_catch.innerHTML=currentUser.company.catchPhrase;
-  
 })
 
 
 
-//Append the data of the table with user div 
-userDiv.appendChild(table);
-
-}
 
 
-// async await with more exericse and dom manipulation 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Displaying the data for the selected employee
+let displayEmployees=(employees)=>{
+
+  //select table body  to target the data 
+  let tableBody=document.querySelector("#table-body");
+
+  //lets create one table row which is initially empty 
+  let tableRow="";
+
+  for(let employee of employees){
+    tableRow +=`<tr>
+   <td>${employee.id}</td>
+    <td>${employee.first_name}</td>
+    <td>${employee.last_name}</td>
+    <td>${employee.email}</td>
+    <td>${employee.gender}</td> 
+    <td>${employee.ip_address}</td> 
+    </tr>`;
+    tableBody.innerHTML=tableRow
+  }
+};
